39348c5f19ac6c0134cd2b321560035d71480865
Jason Falk <jasonfalk9@gmail.com>
2023-01-23 15:54:55 -0800

    Added updatedArray and added ul on submit to show array items.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function favoriteThings(event) {
+function favoriteThingsBusiness(event) {
   event.preventDefault();
   let favoriteBreakfast = document.querySelector("input[name='breakfast']:checked").value;
   let movieInput = document.querySelector("input[name='movie']:checked").value;
@@ -7,16 +7,28 @@ function favoriteThings(event) {
   let stateInput = document.querySelector("input[name='state']:checked").value;
 
   const favoriteThingsArray = [favoriteBreakfast, movieInput, langInput, sportInput, stateInput];
-  console.log(favoriteThingsArray);
-  console.log(favoriteThingsArray[0]);
-  console.log(favoriteThingsArray[1]);
-  console.log(favoriteThingsArray[2]);
+  favoriteThingsArray[1];
+  favoriteThingsArray[0];
+  favoriteThingsArray[2];
 
-  
+  const updatedArray = [];
+  updatedArray.push(favoriteThingsArray[1], favoriteThingsArray[0], favoriteThingsArray[2]);
+  console.log(updatedArray);
+
+  const favoriteUl = document.createElement("ul");
+  favoriteUl.setAttribute("id", "favoriteThings");
+  document.querySelector("form").append(favoriteUl);
+  const liOne = document.createElement("li");
+  const liTwo = document.createElement("li");
+  const liThree = document.createElement("li");
+  liOne.append(updatedArray[0]);
+  liTwo.append(updatedArray[1]);
+  liThree.append(updatedArray[2]);
+  document.getElementById("favoriteThings").append(liOne, liTwo, liThree);
 }
 
 window.addEventListener("load", function() {
   const formInputs = this.document.getElementById("form-inputs");
-  formInputs.addEventListener("submit", favoriteThings);
+  formInputs.addEventListener("submit", favoriteThingsBusiness);
 });
 
